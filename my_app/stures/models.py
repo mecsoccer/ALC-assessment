3f0ef627e642760c31d775023bcc745be1696109
mecsoccer <mecsoccerguy@gmail.com>
2017-11-02 15:16:20 +0000

    Update models.py

--- my_app/stures/models.py
+++ my_app/stures/models.py
@@ -10,7 +10,7 @@ class Student(models.Model):
     sex = models.CharField(max_length=10)
     passport = models.FileField(default=0)
     age = models.IntegerField(default=0)
-    cgpa = models.FloatField()
+    cgpa = models.FloatField(default=5.0)
 
     def get_absolute_url(self):
         return reverse('stures:student')
@@ -22,7 +22,12 @@ class Student(models.Model):
 class Resource(models.Model):
     type = models.CharField(max_length=20)
     author = models.CharField(max_length=30)
-    image = models.FileField(default=0)
+    topic = models.CharField(max_length=100)
+    content = models.CharField(max_length=3000)
+    date = models.CharField(max_length=20)
+
+    def get_absolute_url(self):
+        return reverse('stures:resource')
 
     def __str__(self):
         return self.type + self.author
